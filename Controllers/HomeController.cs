7af9dd1186ac4629f11c06974fc86a4ec3ede1bf
nsfrahmann <71308487+nsfrahmann@users.noreply.github.com>
2020-10-13 10:49:11 -0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FizzBuzz_MVC.Models;
+using System.Text;
+
+namespace FizzBuzz_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Solve()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Solve(string input1, string input2)
+        {
+            var fizzNum = Convert.ToInt32(input1);
+            var buzzNum = Convert.ToInt32(input2);
+            var output = new StringBuilder();
+            for (int index = 1; index <= 100; index++)
+            {
+                output.AppendLine("");
+                if (index % fizzNum == 0 && index % buzzNum ==0)
+                {
+                    output.AppendLine("FizzBuzz");
+                }
+                else if (index % fizzNum == 0)
+                {
+                    output.AppendLine("Fizz");
+                }
+                else if (index % buzzNum == 0)
+                {
+                    output.AppendLine("Buzz");
+                }
+                else
+                {
+                    output.AppendLine($"{index}");
+                }
+            };
+            ViewData["Output"] = output.ToString();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FizzBuzz_MVC
+ Here's my first attempt at using c# in the MVC instead of javascript to solve the FizzBuzz coding challenge
